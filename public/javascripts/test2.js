ffb4fe2a77d6b13a406459d8bd0fc3800334c6b0
Zhu Jiawei <ab052151zhujiawei@ab-insurance.com>
2018-05-04 11:02:48 +0800

    Add non-new construct code snippets

--- public/javascripts/test2.js
+++ public/javascripts/test2.js
@@ -1,25 +1,28 @@
-console.log("Load test1.js");
+console.log("Load test2.js");
 
 var aQuery = function(selector, context) {
-    return aQuery.prototype.init();
+    return new aQuery.prototype.init();
 }
 aQuery.prototype = {
  init: function() {
-     this.age = 18
-     console.log(this);
+    this.age = 18
+    console.log(this);
      return this;
  },
  name: function() {
-     return "name()";
- },
+    return "name()";
+},
  age: 20
 }
+
+aQuery.prototype.init.prototype = aQuery.prototype;
 
 // console.log(aQuery().prototype === aQuery.prototype);
 
 // 18
 console.log(aQuery().age)
 
+//Uncaught TypeError: Object [object Object] has no method 'name' 
 console.log(aQuery().name())
 
 console.log("end");
